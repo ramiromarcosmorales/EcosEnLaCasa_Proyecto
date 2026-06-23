c6e2be36400219c1f2f5680f803d5059317ed888
Sebasthian Harika <sebasthianharika@gmail.com>
2026-06-22 21:16:47 -0300

    feat: escenas creadas y navegacion MainMenu a GameScene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
