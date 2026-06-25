422ffd8d9648e29d9f336bdec828e8f96e3ff135
Sebasthian Harika <sebasthianharika@gmail.com>
2026-06-25 20:21:25 -0300

    feat: navegación MainMenu a Zaguan con Sceneloader y hotspot

---
+++ Assets/Scripts/HotsPot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Hotspot : MonoBehaviour
+{
+    public string sceneDestino;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseDown()
+    {
+        if (sceneDestino != "")
+        {
+            SceneLoader.Instance.LoadScene(sceneDestino);
+        }
+    }
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,18 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-    }
-
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    public void LoadGameScene()
-    {
-        SceneManager.LoadScene("GameScene");
     }
 }
