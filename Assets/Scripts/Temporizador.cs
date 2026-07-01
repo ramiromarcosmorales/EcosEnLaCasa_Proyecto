e9b7016664eacaeea0bcbc02e68f49f153c7e9c5
Sebasthian Harika <sebasthianharika@gmail.com>
2026-07-01 19:05:55 -0300

    feat: temporizador oculto de 60 minutos con final alternativo

---
+++ Assets/Scripts/Temporizador.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Temporizador : MonoBehaviour
+{
+    public static Temporizador Instance;
+
+    private float tiempoLimite = 3600f; // 60 minutos en segundos
+    private float tiempoTranscurrido = 0f;
+    private bool corriendo = false;
+    private bool finalAlternativoDisparado = false;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        IniciarTemporizador();
+    }
+
+    void Update()
+    {
+        if (!corriendo || finalAlternativoDisparado) return;
+
+        tiempoTranscurrido += Time.deltaTime;
+
+        if (tiempoTranscurrido >= tiempoLimite)
+        {
+            corriendo = false;
+            finalAlternativoDisparado = true;
+            DispararFinalAlternativo();
+        }
+    }
+
+    public void IniciarTemporizador()
+    {
+        tiempoTranscurrido = 0f;
+        corriendo = true;
+        finalAlternativoDisparado = false;
+    }
+
+    public float GetTiempoTranscurrido()
+    {
+        return tiempoTranscurrido;
+    }
+
+    public bool EstaCorriendoo()
+    {
+        return corriendo;
+    }
+
+    private void DispararFinalAlternativo()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
